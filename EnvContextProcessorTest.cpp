f7b16303f870e822b355d5dd1e32ffca6ef44332
ratanasv <ratanasv@eecs.oregonstate.edu>
2014-05-12 11:43:46 -0700

    add unit test build config. for some reason using module .def file results in an invalid win32 app.

---
+++ EnvContextProcessorTest.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include <boost/test/unit_test.hpp>
+#include "envcontext_processor.h"
+
+BOOST_AUTO_TEST_SUITE(SHP3DProcessorTestSuite)
+
+
+BOOST_AUTO_TEST_CASE(MyTest) {
+	BOOST_REQUIRE(true);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
---
+++ main.cpp
@@ -0,0 +1,5 @@
+#include "stdafx.h"
+#ifdef ENVISTAS_TEST
+#define BOOST_TEST_MAIN
+#include <boost/test/unit_test.hpp>
+#endif
